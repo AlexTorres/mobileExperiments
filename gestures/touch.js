395fe7aee1774cd335d58be6a84b2ad20469b858
John Torres <alexander.torres@imaginamos.co>
2012-02-23 16:10:46 -0500

    Gestures added

---
+++ gestures/touch.js
@@ -0,0 +1,43 @@
+$(document).on("ready", arranque);
+var startRotation;
+var startScale;
+function arranque()
+{
+
+
+var element = document.getElementById("element")
+
+
+		element.addEventListener("gesturestart", onGestureStart, false);
+		element.addEventListener("gesturechange", onGestureChange, false);
+		element.addEventListener("gestureend", onGestureEnd, false);
+
+}
+
+function onGestureStart(event)
+{
+	event.preventDefault();
+	startRotation=event.rotation;
+	startScale=event.scale;
+	$("#trace h6").replaceWith('<h6>Start touch</h6>');
+
+}
+function onGestureChange(event)
+{
+	
+	    event.preventDefault();
+ 
+    event.target.style.webkitTransform =
+        'scale(' + event.scale  + startScale  + ') rotate(' + event.rotation + startRotation + 'deg)';
+
+        
+
+
+        
+	
+}
+function onGestureEnd()
+{
+
+	$("#trace h6").replaceWith('<h6>Start end</h6>');
+}
---
+++ touch/touch.js
@@ -0,0 +1,41 @@
+$(document).on("ready", arranque);
+var startX;
+var startY;
+function arranque()
+{
+
+
+var element = document.getElementById("element")
+		element.addEventListener("touchstart", onTouchStart);
+		element.addEventListener("touchmove", onTouchMove);
+		element.addEventListener("touchend", onTouchEnd);
+
+}
+
+function onTouchStart(event)
+{
+	startX=event.targetTouches[0].pageX ;
+	startY=event.targetTouches[0].pageY ;
+	$("#trace h6").replaceWith('<h6>Start touch</h6>');
+
+}
+function onTouchMove(event)
+{
+	
+	    event.preventDefault();
+    var curX = event.targetTouches[0].pageX - startX;
+    var curY = event.targetTouches[0].pageY - startY;
+    event.targetTouches[0].target.style.webkitTransform =
+        'translate(' + curX + 'px, ' + curY + 'px)';
+
+        
+
+
+        
+	
+}
+function onTouchEnd()
+{
+
+	$("#trace h6").replaceWith('<h6>Start end</h6>');
+}
