6cb9daf17607335454879078e17fcf46084169be
John Alexander Torres <jalexandert@gmail.com>
2012-02-25 14:46:04 -0500

    Update touch/touch.js

--- touch/touch.js
+++ touch/touch.js
@@ -3,13 +3,10 @@ var startX;
 var startY;
 function arranque()
 {
-
-
-var element = document.getElementById("element")
-		element.addEventListener("touchstart", onTouchStart);
-		element.addEventListener("touchmove", onTouchMove);
-		element.addEventListener("touchend", onTouchEnd);
-
+	var element = document.getElementById("element")
+	element.addEventListener("touchstart", onTouchStart);
+	element.addEventListener("touchmove", onTouchMove);
+	element.addEventListener("touchend", onTouchEnd);
 }
 
 function onTouchStart(event)
@@ -17,25 +14,17 @@ function onTouchStart(event)
 	startX=event.targetTouches[0].pageX ;
 	startY=event.targetTouches[0].pageY ;
 	$("#trace h6").replaceWith('<h6>Start touch</h6>');
-
 }
 function onTouchMove(event)
 {
-	
-	    event.preventDefault();
+	event.preventDefault();
     var curX = event.targetTouches[0].pageX - startX;
     var curY = event.targetTouches[0].pageY - startY;
     event.targetTouches[0].target.style.webkitTransform =
-        'translate(' + curX + 'px, ' + curY + 'px)';
-
+    'translate(' + curX + 'px, ' + curY + 'px)';
         
-
-
-        
-	
 }
 function onTouchEnd()
 {
-
 	$("#trace h6").replaceWith('<h6>Start end</h6>');
 }
