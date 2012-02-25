af44a9db87fd21aac629fb31dbd469ede29e62b1
John Alexander Torres <jalexandert@gmail.com>
2012-02-25 14:56:29 -0500

    Update acelerometer/touch.js

--- acelerometer/touch.js
+++ acelerometer/touch.js
@@ -3,16 +3,11 @@ var startRotation;
 var startScale;
 function arranque()
 {
-
-
-var element = document.getElementById("element")
-
-if (window.DeviceMotionEvent != undefined) {
-	window.ondevicemotion  = onDeviceMotion;
-
-}
-		
-
+	var element = document.getElementById("element")
+	if (window.DeviceMotionEvent != undefined) 
+	{
+		window.ondevicemotion  = onDeviceMotion;
+	}
 }
 
 function onDeviceMotion(event)
@@ -20,12 +15,8 @@ function onDeviceMotion(event)
 	var accelerationX = event.accelerationIncludingGravity.x;  
     var accelerationY = event.accelerationIncludingGravity.y;  
     var accelerationZ = event.accelerationIncludingGravity.z;
-
-element.style.webkitTransform ='rotate(' + accelerationX*28 + 'deg)';
-       
-        document.getElementById("alpha").innerHTML ='alpha' +Math.round(event.rotationRate.alpha) ;
-        document.getElementById("beta").innerHTML = 'beta' + Math.round(event.rotationRate.beta);
-        document.getElementById("gamma").innerHTML = 'gamma' + Math.round(event.rotationRate.gamma);
-
-
+	element.style.webkitTransform ='rotate(' + accelerationX*28 + 'deg)';
+    document.getElementById("alpha").innerHTML ='alpha' +Math.round(event.rotationRate.alpha) ;
+    document.getElementById("beta").innerHTML = 'beta' + Math.round(event.rotationRate.beta);
+    document.getElementById("gamma").innerHTML = 'gamma' + Math.round(event.rotationRate.gamma);
 }
