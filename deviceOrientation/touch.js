5ed6376dafde577a2e027d0bf5991ce2bc21b66f
John Alexander Torres <jalexandert@gmail.com>
2012-02-25 14:50:28 -0500

    Update deviceOrientation/touch.js

--- deviceOrientation/touch.js
+++ deviceOrientation/touch.js
@@ -3,14 +3,10 @@ var startRotation;
 var startScale;
 function arranque()
 {
-
-
-var element = document.getElementById("element")
-
-
-		element.addEventListener("gesturestart", onGestureStart, false);
-		element.addEventListener("gesturechange", onGestureChange, false);
-		element.addEventListener("gestureend", onGestureEnd, false);
+	var element = document.getElementById("element")
+	element.addEventListener("gesturestart", onGestureStart, false);
+	element.addEventListener("gesturechange", onGestureChange, false);
+	element.addEventListener("gestureend", onGestureEnd, false);
 
 }
 
@@ -24,20 +20,11 @@ function onGestureStart(event)
 }
 function onGestureChange(event)
 {
-	
-	    event.preventDefault();
- 
-    event.target.style.webkitTransform =
-        'scale(' + event.scale  + startScale  + ') rotate(' + event.rotation + startRotation + 'deg)';
-
-        
-
-
-        
-	
+	event.preventDefault();
+ 	event.target.style.webkitTransform =
+    'scale(' + event.scale  + startScale  + ') rotate(' + event.rotation + startRotation + 'deg)';
 }
 function onGestureEnd()
 {
-
 	$("#trace h6").replaceWith('<h6>Start end</h6>');
 }
