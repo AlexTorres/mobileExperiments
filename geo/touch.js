1e93dbb514d900bac6664e6447f94e1208e1784e
John Alexander Torres <jalexandert@gmail.com>
2012-02-25 14:58:47 -0500

    Update geo/touch.js

--- geo/touch.js
+++ geo/touch.js
@@ -3,20 +3,15 @@ var startRotation;
 var startScale;
 function arranque()
 {
-
-
-var element = document.getElementById("element")
-
-
-		 navigator.geolocation.getCurrentPosition(showMap);
+	var element = document.getElementById("element")
+	navigator.geolocation.getCurrentPosition(showMap);
 
 }
 
- function showMap(position) 
- {
-      // Show a map centered at position
-       	var latitude = position.coords.latitude;
-    	var longitude = position.coords.longitude;
-    	document.getElementById("lat").innerHTML = 'latitude= ' + latitude;
-    	document.getElementById("long").innerHTML = 'longitude= ' + longitude;
-  }
+function showMap(position) 
+{
+ 	var latitude = position.coords.latitude;
+    var longitude = position.coords.longitude;
+    document.getElementById("lat").innerHTML = 'latitude= ' + latitude;
+    document.getElementById("long").innerHTML = 'longitude= ' + longitude;
+ }
